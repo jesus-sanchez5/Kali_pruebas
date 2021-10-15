2610a7fcb7dfc71ed9e6bfb0c6d80eb246c5b784
jesus <chusete777@gmail.com>
2021-10-15 17:00:08 +0200

    Pruebas

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def hola(num):
+    print(f'{num}')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    hola(4)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
