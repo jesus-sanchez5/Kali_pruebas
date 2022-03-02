6f314d09a376bb928d7e9834ba59672a0eebfbd6
Jesus <chusete777@gmail.com>
2022-03-02 10:24:37 -0500

    Archivos

---
+++ kali/programa prueba/file.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+	setuid(0);
+	printf("\n\n Listado de procesos (/usr/bin/ps)\n\n");
+	system("/usr/bin/ps");
+	printf("\n\n Listado procesos (ps)\n\n");
+	system("ps");
+}
