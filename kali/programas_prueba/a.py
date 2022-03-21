b6a1afe4b969ae704566379da1bf896ae6f11ea7
Jesus Sanchez <chusete777@gmail.com>
2022-03-21 12:55:55 +0100

    p.py

---
+++ kali/programas_prueba/a.py
@@ -0,0 +1,26 @@
+from os import system
+import platform, subprocess
+import os, datetime
+
+sistema=""
+
+def sistemaOP():
+    global sistema
+    sistema = platform.system()
+    print ("Sistema operativo: ", sistema)
+
+def pingwindows(): 
+    a = system('ping -n 4 172.19.131.130 | find "recibidos = 0"')
+    print (a)
+   
+
+#if len(sys.argv)!=2:
+#	print ("Error: Parametros erroneos\nUso:python3 " + sys.argv[1] + " ip-address")
+#	exit(1)
+
+if __name__=='__main__':
+    print ('Estoy en el main')
+    sistemaOP()
+    if (sistema == 'Windows'):
+        #pingwindows()
+        print ("Hola")
