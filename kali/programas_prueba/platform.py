1e5f5de1dedd670165822f21b06d22c8bba47623
Jesus Sanchez <chusete777@gmail.com>
2022-04-07 11:51:33 +0200

    plat

---
+++ kali/programas_prueba/platform.py
@@ -0,0 +1,10 @@
+import platform
+
+print(f"Compueter network {platform.node()}")
+print(f"Machine type{platform.machine()}")
+print(f"Processor type {platform.processor()}")
+print(f"Platform {platform.platform()}")
+print(f"Operating  {platform.system()}")
+print(f"Release: {platform.release()}")
+print(f"Version {platform.version()}")
+
