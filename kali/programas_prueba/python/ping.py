e60b93cf4d9b057010949615e3a75c302ed21928
Jesus Sanchez <38619331+jesus-sanchez5@users.noreply.github.com>
2022-07-23 11:21:19 +0200

    #

--- kali/programas_prueba/python/ping.py
+++ kali/programas_prueba/python/ping.py
@@ -1,6 +1,9 @@
+from concurrent.futures import process
 import re, sys, subprocess
 
 
 if __name__ == '__main__':
-
-	proc = subprocess.Popen(["/usr/bin/ping -c 1 8.8.8.8"], stdout= subprocess.PIPE, shell=True)
+	ip="8.8.8.8"
+	proc = subprocess.Popen(["ping -c 1 %s" % ip, ""], stdout= subprocess.PIPE, shell=True)
+	print(proc.communicate())
+   
