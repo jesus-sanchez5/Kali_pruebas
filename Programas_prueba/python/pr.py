f6e933f0f3f3ebec28467a8a354c9fdaa6bca314
Jesus Sanchez <38619331+jesus-sanchez5@users.noreply.github.com>
2022-08-04 21:43:08 +0200

    pr

--- Programas_prueba/python/pr.py
+++ Programas_prueba/python/pr.py
@@ -1,2 +1,23 @@
-from plyer import notification
-notification.notify(title='Noti',message='holaaaa',app_icon=None,timeout=20)
+# from plyer import notification
+# notification.notify(title='Noti',message='holaaaa',app_icon=None,timeout=20)
+
+
+
+# import os
+
+# def notify(title, text):
+    # os.system("""
+            #   osascript -e 'display notification "{}" with title "{}"'
+            #   """.format(text, title))
+
+# notify("Title", "Heres an alert")
+
+
+import psutil, os
+battery = psutil.sensors_battery()
+percentage = battery.percent
+print(f"Our system has {percentage} percent ")
+def notify(title, text):
+     os.system("""osascript -e 'display notification "{}" with title "{}"'""".format(text, title))
+
+notify("Title {percentage}", "Heres an alert '{percentage}'")
