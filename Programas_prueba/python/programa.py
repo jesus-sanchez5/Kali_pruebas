e50ed0a960ce841d6519e83e15dd676ccd9439b7
Jesus Sanchez <38619331+jesus-sanchez5@users.noreply.github.com>
2022-08-07 23:11:39 +0200

    subprocess

--- Programas_prueba/python/programa.py
+++ Programas_prueba/python/programa.py
@@ -11,8 +11,6 @@ def obtenerInterfaces():
     proc = subprocess.Popen(["ifconfig -s | awk 'NR>1{print $1}'", ""], stdout= subprocess.PIPE, shell=True)
     (out,err) = proc.communicate()
     out = out.split()
-    #pattern = r'\w+'
-    #print(out)
     print(f"Interfaces:")
     for salida in out:
         interfaces.append(out[x].decode('utf-8'))
@@ -31,13 +29,10 @@ def elegirInterfaz():
         if respuesta == 'Y' or respuesta =='y' or respuesta == 'yes' or respuesta == 'Yes':
             subprocess.Popen(["sudo airmon-ng check kill", ""], stdout= subprocess.PIPE, shell=True)
             subprocess.Popen(["sudo ifconfig %s down " % inter, ""], stdout= subprocess.PIPE, shell=True)
-            
-            """ os.system('sudo airmon-ng check kill')
-            os.system('sudo ifconfig '+inter+' down ')
-            os.system('sudo airmon-ng check kill')
-            os.system('sudo iwconfig '+inter+' mode monitor')
-            os.system('sudo ifconfig '+inter+' up')
-            os.system('iwconfig '+inter) """
+            subprocess.Popen(["sudo airmon-ng check kill", ""], stdout= subprocess.PIPE, shell=True)
+            subprocess.Popen(["sudo iwconfig %s mode monitor" % inter, ""], stdout= subprocess.PIPE, shell=True)
+            subprocess.Popen(["sudo ifconfig %s up" % inter, ""], stdout= subprocess.PIPE, shell=True)
+            os.system('iwconfig '+inter) 
     else:
         print("No existe esta interfaz")
 
