9452d3fd0ef91fc96e62f2cc18c01f70c494e818
Jesus Sanchez <38619331+jesus-sanchez5@users.noreply.github.com>
2022-09-29 19:10:06 +0200

    kj

---
+++ Programas_prueba/python/pruebasWifi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+
+
+def ayuda():
+    print("Ayuda\n\tEl programa se ejecuta de la siguiente manera 'python3 pruebasWifi.py' ")
+    
+def dependencias():
+    print("Dependencias")
+    
+    
+ayuda()
+dependencias()
