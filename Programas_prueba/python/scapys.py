55eec790339439da37b949552baf744f8a602144
Jesus Sanchez <38619331+jesus-sanchez5@users.noreply.github.com>
2022-10-30 10:09:42 +0100

    s

--- Programas_prueba/python/scapys.py
+++ Programas_prueba/python/scapys.py
@@ -1,4 +1,11 @@
-import scapy.all as scapy
-  
-request = scapy.ARP()
-print(request.summary())
+
+
+
+ #! /usr/bin/env python
+import sys
+from scapy.all import sr1,IP,ICMP
+ 
+p=sr1(IP(dst=sys.argv[1])/ICMP())
+if p:
+     p.show()
+     sr1(p)
