59532c6483e978312f8b12d414cbc037d11dc09d
jesus <chusete777@gmail.com>
2022-10-30 10:32:05 +0100

    COMENTARIO

--- Programas_prueba/python/pa.py
+++ Programas_prueba/python/pa.py
@@ -1,26 +1,9 @@
-from os import system
-import platform, subprocess
-import os, datetime
+#! /usr/bin/env python
 
-sistema=""
+import sys
+from scapy.all import sr1,IP,ICMP
 
-def sistemaOP():
-    global sistema
-    sistema = platform.system()
-    print ("Sistema operativo: ", sistema)
-
-def pingwindows(): 
-    a = subprocess.run('ping -n 4 192.168.1.1"')
-    print (a)
-   
-
-#if len(sys.argv)!=2:
-#	print ("Error: Parametros erroneos\nUso:python3 " + sys.argv[1] + " ip-address")
-#	exit(1)
-
-if __name__=='__main__':
-    print ('Estoy en el main')
-    sistemaOP()
-    if (sistema == 'Windows'):
-        pingwindows()
-       
+p=sr1(IP(dst=sys.argv[1])/ICMP())
+if p:
+    p.show()
+    sr1(p)
---
+++ git.sh
@@ -0,0 +1,7 @@
+git add .
+sleep 2
+git status
+sleep 2
+git commit -m "COMENTARIO"
+sleep 1
+git push origin master
